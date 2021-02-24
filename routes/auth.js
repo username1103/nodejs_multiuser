0e71c613770177cf47c3cbb8ae424bedfa4c6de7
username1103 <username1103@naver.com>
2021-02-24 23:28:51 +0900

    Add register UI

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const template = require("../lib/template");
+const auth = require("../lib/auth");
 
 module.exports = function (passport) {
   router.get("/login", (req, res) => {
@@ -15,19 +16,15 @@ module.exports = function (passport) {
       title,
       list,
       `
-    <div class="feedback">${feedback}</div>
-    <form action="/auth/login" method="post">
+      <div class="feedback">${feedback}</div>
+      <form action="/auth/login" method="post">
         <p><input type="text" name="email" placeholder="email"></p>
-        <p>
-            <input type="password" name="password" placeholder="password">
-        </p>
-        <p>
-        <input type="submit">
-        </p>
-    </form>
-    `,
+        <p><input type="password" name="password" placeholder="password"></p>
+        <p><input type="submit"></p>
+      </form>
+      `,
       "",
-      `<a href="/auth/login">login</a>`
+      auth.statusUI(req)
     );
     res.send(html);
   });
@@ -45,6 +42,44 @@ module.exports = function (passport) {
   router.get("/logout", (req, res) => {
     req.logout();
     res.redirect("/");
+  });
+
+  router.get("/register", (req, res) => {
+    const fmsg = req.flash();
+    let feedback = "";
+    if (fmsg.error) {
+      feedback = fmsg.error[0];
+    }
+    const title = "WEB - 회원가입";
+    const list = template.list(req.list);
+    var html = template.HTML(
+      title,
+      list,
+      `
+      <div class="feedback">${feedback}</div>
+      <form action="/auth/register" method="post">
+        <p><input type="text" name="email" placeholder="email"></p>
+        <p><input type="password" name="password" placeholder="password"></p>
+        <p><input type="password" name="password_confirm" placeholder="password"></p>
+        <p><input type="text" name="displayName" placeholder="nickname"></p>
+        <p><input type="submit" value="등록"></p>
+      </form>
+      `,
+      "",
+      auth.statusUI(req)
+    );
+    res.send(html);
+  });
+
+  router.post("/register", (req, res) => {
+    var post = req.body;
+    var email = post.email;
+    var password = post.password;
+    var password_confirm = post.password_confirm;
+    var displayName = post.displayName;
+    fs.writeFile(`./data/${title}`, description, "utf8", function (err) {
+      res.redirect(`/topic/${title}`);
+    });
   });
 
   return router;
