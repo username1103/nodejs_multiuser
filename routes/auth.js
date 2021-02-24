b3ebbfeb381bb15d3f231b6f0ca3933e2ee254fa
username1103 <username1103@naver.com>
2021-02-25 00:24:38 +0900

    Add checking register data

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,16 @@ const router = express.Router();
 const template = require("../lib/template");
 const auth = require("../lib/auth");
 
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
+
+const adapter = new FileSync("db.json");
+const db = low(adapter);
+db.defaults({ user: [] }).write();
+
+const shortid = require("shortid");
+const { nextElementSibling } = require("domutils");
+
 module.exports = function (passport) {
   router.get("/login", (req, res) => {
     const fmsg = req.flash();
@@ -18,8 +28,8 @@ module.exports = function (passport) {
       `
       <div class="feedback">${feedback}</div>
       <form action="/auth/login" method="post">
-        <p><input type="text" name="email" placeholder="email"></p>
-        <p><input type="password" name="password" placeholder="password"></p>
+        <p><input type="text" name="email" placeholder="이메일"></p>
+        <p><input type="password" name="password" placeholder="비밀번호"></p>
         <p><input type="submit"></p>
       </form>
       `,
@@ -58,10 +68,10 @@ module.exports = function (passport) {
       `
       <div class="feedback">${feedback}</div>
       <form action="/auth/register" method="post">
-        <p><input type="text" name="email" placeholder="email"></p>
-        <p><input type="password" name="password" placeholder="password"></p>
-        <p><input type="password" name="password_confirm" placeholder="password"></p>
-        <p><input type="text" name="displayName" placeholder="nickname"></p>
+        <p><input type="text" name="email" placeholder="이메일"></p>
+        <p><input type="password" name="password" placeholder="비밀번호"></p>
+        <p><input type="password" name="password_confirm" placeholder="비밀번호확인"></p>
+        <p><input type="text" name="displayName" placeholder="이름"></p>
         <p><input type="submit" value="등록"></p>
       </form>
       `,
@@ -77,9 +87,36 @@ module.exports = function (passport) {
     var password = post.password;
     var password_confirm = post.password_confirm;
     var displayName = post.displayName;
-    fs.writeFile(`./data/${title}`, description, "utf8", function (err) {
-      res.redirect(`/topic/${title}`);
-    });
+
+    if (email === "" || password === "" || displayName === "") {
+      req.flash("error", "모든 값을 채워주세요.");
+      res.redirect("/auth/register");
+    } else if (db.get("user").find({ email: email }).value() !== undefined) {
+      req.flash("error", "이미 존재하는 이메일 입니다.");
+      res.redirect("/auth/register");
+    } else if (password !== password_confirm) {
+      req.flash("error", "비밀번호와 비밀번화 확인 값이 다릅니다.");
+      res.redirect("/auth/register");
+    } else if (
+      db.get("user").find({ displayName: displayName }).value() !== undefined
+    ) {
+      req.flash("error", "이미 존재하는 닉네임 입니다.");
+      res.redirect("/auth/register");
+    } else {
+      const user = {
+        id: shortid.generate(),
+        email: email,
+        password: password,
+        displayName: displayName,
+      };
+      db.get("user").push(user).write();
+      req.login(user, (err) => {
+        if (err) {
+          return next(err);
+        }
+        return res.redirect("/");
+      });
+    }
   });
 
   return router;
