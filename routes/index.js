4b2ffa3598048aa310dce2e8b619f835d127c5ee
username1103 <username1103@naver.com>
2021-02-26 00:55:40 +0900

    Change Update and Delete using lowdb

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,12 @@ const template = require("../lib/template");
 const auth = require("../lib/auth");
 
 router.get("/", (req, res) => {
+  const fmsg = req.flash();
+  let feedback = "";
+  if (fmsg.error) {
+    feedback = fmsg.error[0];
+  }
+
   var title = "Welcome";
   var description = "Hello, Node.js";
   var list = template.list(req.list);
@@ -12,7 +18,10 @@ router.get("/", (req, res) => {
     list,
     `<h2>${title}</h2>${description}
       <img src="/images/coding.jpg">`,
-    `<a href="/topic/create">create</a>`,
+    `
+    <div class="feedback">${feedback}</div>
+    <a href="/topic/create">create</a>
+    `,
     auth.statusUI(req)
   );
   res.send(html);
--- routes/topic.js
+++ routes/topic.js
@@ -10,8 +10,8 @@ const shortid = require("shortid");
 
 router.get("/create", (req, res) => {
   if (!auth.isOwner(req)) {
-    res.redirect("/");
-    return false;
+    req.flash("error", "로그인해야 사용 가능합니다.");
+    return res.redirect("/");
   }
   var title = "WEB - create";
   var list = template.list(req.list);
@@ -20,8 +20,8 @@ router.get("/create", (req, res) => {
     list,
     `
     <form action="/topic/create" method="post">
-      <p><input type="text" name="title" placeholder="title"></p>
-      <p><textarea name="description" placeholder="description"></textarea></p>
+      <p><input type="text" name="title" placeholder="제목"></p>
+      <p><textarea name="description" placeholder="내용"></textarea></p>
       <p><input type="submit"></p>
     </form>
     `,
@@ -33,82 +33,93 @@ router.get("/create", (req, res) => {
 
 router.post("/create", (req, res) => {
   if (!auth.isOwner(req)) {
-    res.redirect("/");
-    return false;
+    req.flash("error", "로그인해야 사용 가능합니다.");
+    return res.redirect("/");
   }
   var post = req.body;
   var title = post.title;
   var description = post.description;
   const id = shortid.generate();
+  const date = new Date();
   db.get("topics")
     .push({
       id: id,
       title: title,
       description: description,
       user_id: req.user.id,
-      date: new Date(),
+      createdate: date,
+      updatedate: date,
     })
     .write();
   res.redirect(`/topic/${id}`);
 });
 
 router.get("/update/:pageId", (req, res) => {
+  console.log(path.parse(req.params.pageId).base);
   if (!auth.isOwner(req)) {
-    res.redirect("/");
-    return false;
+    req.flash("error", "로그인해야 이용가능 합니다.");
+    return res.redirect(`/topic/${req.params.pageId}`);
   }
-  var filteredId = req.params.pageId;
-  fs.readFile(`./data/${filteredId}`, "utf8", function (err, description) {
-    var title = filteredId;
-    var list = template.list(req.list);
-    var html = template.HTML(
-      title,
-      list,
-      `
-      <form action="/topic/update" method="post">
-        <input type="hidden" name="id" value="${title}">
-        <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-        <p><textarea name="description" placeholder="description">${description}</textarea></p>
-        <p><input type="submit"></p>
-      </form>
+  let topic = db.get("topics").find({ id: req.params.pageId }).value();
+  if (req.user.id !== topic.user_id) {
+    req.flash("error", "자신이 쓴 글만 수정가능합니다.");
+    return res.redirect(`/topic/${topic.id}`);
+  }
+  const title = topic.title;
+  const description = topic.description;
+  const list = template.list(req.list);
+  const html = template.HTML(
+    title,
+    list,
+    `
+    <form action="/topic/update" method="post">
+      <input type="hidden" name="id" value="${topic.id}">
+      <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+      <p><textarea name="description" placeholder="description">${description}</textarea></p>
+      <p><input type="submit"></p>
+    </form>
       `,
-      `<a href="/topic/create">create</a> <a href="/topic/update/${title}">update</a>`,
-      auth.statusUI(req)
-    );
-    res.send(html);
-  });
+    `<a href="/topic/create">create</a> <a href="/topic/update/${topic.id}">update</a>`,
+    auth.statusUI(req)
+  );
+  res.send(html);
 });
 
 router.post("/update", (req, res) => {
-  if (!auth.isOwner(req)) {
-    res.redirect("/");
-    return false;
-  }
   var post = req.body;
   var id = post.id;
   var title = post.title;
   var description = post.description;
-  fs.rename(`./data/${id}`, `data/${title}`, function (error) {
-    fs.writeFile(`./data/${title}`, description, "utf8", function (err) {
-      res.redirect(`/topic/${title}`);
-    });
-  });
+  db.get("topics")
+    .find({ id: id })
+    .assign({ title: title, description: description, updatedate: new Date() })
+    .write();
+  res.redirect(`/topic/${id}`);
 });
 
 router.post("/delete", (req, res) => {
-  if (!auth.isOwner(req)) {
-    res.redirect("/");
-    return false;
-  }
   var post = req.body;
   var id = post.id;
-  var filteredId = path.parse(id).base;
-  fs.unlink(`./data/${filteredId}`, function (error) {
-    res.redirect(`/`);
-  });
+  if (!auth.isOwner(req)) {
+    req.flash("error", "로그인해야 이용가능 합니다.");
+    return res.redirect(`/topic/${id}`);
+  }
+  let topic = db.get("topics").find({ id: id }).value();
+  if (req.user.id !== topic.user_id) {
+    req.flash("error", "자신이 쓴 글만 삭제가능합니다.");
+    return res.redirect(`/topic/${id}`);
+  }
+  db.get("topics").remove({ id: id }).write();
+  res.redirect("/");
 });
 
 router.get("/:pageId", (req, res) => {
+  const fmsg = req.flash();
+  let feedback = "";
+  if (fmsg.error) {
+    feedback = fmsg.error[0];
+  }
+
   const topic = db.get("topics").find({ id: req.params.pageId }).value();
   const author = db.get("users").find({ id: topic.user_id }).value();
   const sanitizedTitle = sanitizeHtml(topic.title);
@@ -122,17 +133,18 @@ router.get("/:pageId", (req, res) => {
     `
     <h2>${sanitizedTitle}</h2>
     <div>
-    <p>저자 : ${author.displayName}, 날짜 : ${topic.date}</p>
+    <p>저자 : ${author.displayName}, 작성일 : ${topic.createdate}, 수정일 : ${topic.updatedate}</p>
     </div>
     <div>
     ${sanitizedDescription}
     </div>
     `,
     `
+    <div class="feedback">${feedback}</div>
     <a href="/topic/create">create</a> 
-    <a href="/topic/update/${sanitizeHtml(topic.title)}">update</a>
+    <a href="/topic/update/${topic.id}">update</a>
     <form action="/topic/delete" method="post">
-      <input type="hidden" name="id" value="${sanitizedTitle}">
+      <input type="hidden" name="id" value="${topic.id}">
       <input type="submit" value="delete">
     </form>
     `,
